8ae9712b0b307fed3c32cf524d46a88525a53d69
Heorhii Lytvynenko <104064704+heorhii-lytvynenko@users.noreply.github.com>
2025-12-23 02:03:52 +0100

    basic config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'path';
 
 export default defineConfig({
-  base: '/',
-  plugins: [react(), svgr()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-        @use "@/styles/mixins/fonts.scss" as *;
-        @use "@/styles/mixins/respond.scss" as *;
-        @use "@/styles/mixins/shadows.scss" as *;
-        @use "@/styles/tockens/colors.scss" as *;
-        @use "@/styles/tockens/other.scss" as *;
-        @use "@/styles/animation/animation.scss" as *;
-        @use "@/styles/keyframes/keyframes.scss" as *;
-        `,
-      },
-    },
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+})
